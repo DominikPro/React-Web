be90c2262bc6ee834c06d7c8bdc47c84afdf3cd3
Dominik Proficz <dominik.proficz@gmail.com>
2019-09-17 21:06:53 +0200

    added Header.css with basic styles

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,15 @@
 import React from "react";
+import "../styles/Header.css";
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={img1} alt="city" />
+    </div>
+  );
 };
 
 export default Header;
