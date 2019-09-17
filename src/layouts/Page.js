3bfb3ece3faac34e6a10d292ac24d98901c55653
Dominik Proficz <dominik.proficz@gmail.com>
2019-09-18 00:03:28 +0200

    Page.js routing

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "../pages/HomePage.js";
+import ProductsPage from "../pages/ProductsPage.js";
+import ContactPage from "../pages/ContactPage.js";
+import AdminPage from "../pages/AdminPage.js";
+import ErrorPage from "../pages/ErrorPage.js";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/products" exact component={ProductsPage}></Route>
+        <Route path="/contact" exact component={ContactPage}></Route>
+        <Route path="/admin" exact component={AdminPage}></Route>
+        <Route component={ErrorPage}></Route>
+      </Switch>
+    </>
+  );
 };
 
 export default Page;
