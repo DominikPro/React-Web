0212ee5650a440eb01a4922526d97bc72d1690b0
Dominik Proficz <dominik.proficz@gmail.com>
2019-09-19 00:29:16 +0200

    Article.js component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,35 @@
 import React from "react";
+import Article from "../components/Article";
+const articles = [
+  {
+    id: 1,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum delectus incidunt unde qui expedita voluptas minus, debitis nesciunt adipisci rem tempore deleniti ipsam quam aspernatur dolorem dolorum sed nisi porro."
+  },
+  {
+    id: 2,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum delectus incidunt unde qui expedita voluptas minus, debitis nesciunt adipisci rem tempore deleniti ipsam quam aspernatur dolorem dolorum sed nisi porro."
+  },
+  {
+    id: 3,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum delectus incidunt unde qui expedita voluptas minus, debitis nesciunt adipisci rem tempore deleniti ipsam quam aspernatur dolorem dolorum sed nisi porro."
+  }
+];
+
 const HomePage = () => {
-  return <div>Strona startowa</div>;
+  const artList = articles.map(article => (
+    <Article key={article.id} {...article} />
+  ));
+
+  return <div className="home">{artList}</div>;
 };
 
 export default HomePage;
