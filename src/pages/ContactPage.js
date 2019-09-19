c594da7c1ac906ddcb283c3d626ae703a14d4550
Dominik Proficz <dominik.proficz@gmail.com>
2019-09-20 00:00:50 +0200

    adding ContactPage.css file with basic css styles for form --- protection against leaving the form not sent

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,41 @@
 import React from "react";
-const ContactPage = () => {
-  return <div>Kontakt</div>;
-};
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
+
+class ContactPage extends React.Component {
+  state = {
+    value: ""
+  };
+  handleSubmite = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmite}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomość..."
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="Ma częściowo wypełniony formularz - czy chcesz go porzucić?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
